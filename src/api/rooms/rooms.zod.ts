a27812e249cab30ef58ad14464973fe4543c9098
Max <max.gottschalk@mailbox.org>
2023-07-08 20:03:49 +0200

    fix: move dateQuerySchema

--- src/api/rooms/rooms.zod.ts
+++ src/api/rooms/rooms.zod.ts
@@ -1,31 +1,8 @@
-import { roomTypeSchema, idParamSchema } from 'src/schemas';
+import { roomTypeSchema, idParamSchema, dateQuerySchema } from 'src/schemas';
 import z from 'zod';
-
-export const dateQuerySchema = z
-	.object({
-		startDate: z.coerce.date().optional(),
-		endDate: z.coerce.date().optional()
-	})
-	.superRefine((val, ctx) => {
-		if (val.startDate && !val.endDate) {
-			ctx.addIssue({
-				code: z.ZodIssueCode.custom,
-				message: 'endDate is required if startDate is provided',
-				path: ['endDate']
-			});
-		}
-
-		if (val.endDate && !val.startDate) {
-			ctx.addIssue({
-				code: z.ZodIssueCode.custom,
-				message: 'startDate is required if endDate is provided',
-				path: ['startDate']
-			});
-		}
-	});
 
 export const roomTypeParamSchema = z.object({
 	roomType: roomTypeSchema
 });
 
-export { idParamSchema };
+export { idParamSchema, dateQuerySchema };
