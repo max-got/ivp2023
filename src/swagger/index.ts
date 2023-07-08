630a7d172048650df42afca5c5cef5b32915106f
Max <max.gottschalk@mailbox.org>
2023-07-08 17:55:45 +0200

    fix: generate swagger

--- src/swagger/index.ts
+++ src/swagger/index.ts
@@ -1,6 +1,8 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import swaggerAutogen from 'swagger-autogen';
+import doc from 'src/swagger/doc';
 
 //get ./routes folder
 const __filename = fileURLToPath(import.meta.url);
@@ -12,3 +14,7 @@ export const filesInDirArray = [routes_folder + '/index.ts'];
 
 //output file
 export const outputFile = path.join(__dirname, './swagger_output.json');
+
+swaggerAutogen({ language: 'de-DE' })(outputFile, filesInDirArray, doc).then(async () => {
+	console.log('Swagger JSON file created');
+});
