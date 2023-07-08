916637149c61c6388adf0eea4858dae48c5fd1c8
Max G <64729700+max-got@users.noreply.github.com>
2023-07-08 19:47:50 +0200

    Feat hotels route (#1)
    * fix: generate swagger
    
    * feat: add hotels route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,12 @@
 import express from 'express';
 
-import swaggerAutogen from 'swagger-autogen';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from 'src/swagger/swagger_output.json';
-import { filesInDirArray, outputFile } from 'src/swagger/generator';
-import doc from 'src/swagger/doc';
 
 import rooms_routes from 'src/api/rooms/rooms.routes';
+import hotels_routes from 'src/api/hotels/hotels.routes';
 
 const app = express();
-
-swaggerAutogen({ language: 'de-DE' })(outputFile, filesInDirArray, doc);
 
 //docs
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -24,5 +20,11 @@ app.use(
 	// #swagger.tags = ['Rooms']
 	'/api/rooms',
 	rooms_routes
+);
+
+app.use(
+	'/api/hotels',
+	// #swagger.tags = ['Hotels']
+	hotels_routes
 );
 export default app;
